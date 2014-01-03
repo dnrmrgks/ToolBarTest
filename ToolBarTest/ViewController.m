e08e19c07b646fcd2ff9e52dc5aa17d4bce41c6e
park sung chan <dnrmrgks@naver.com>
2014-01-03 14:53:59 +0900

    툴
    툴바실습

--- ToolBarTest/ViewController.m
+++ ToolBarTest/ViewController.m
@@ -9,14 +9,67 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIToolbar *handleBarButtonClick;
 
 @end
 
 @implementation ViewController
 
+
+
+-(IBAction)handleBarButtonClick:(id)sender{
+    UIBarButtonItem *button = (UIBarButtonItem *)sender;
+    if( 1==button.tag){
+        NSLog(@"왼쪽버튼");
+        
+    }
+    else{
+        NSLog(@"오른쪽버튼");
+    }
+}
+
+-(void)handleRefresh:(id)sender{
+    NSLog(@"Refresh!");
+}
+
+-(void)handleButton:(id)sender{
+    NSLog(@"Button Clicked!");
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    
+    
+    UIToolbar *toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 160, 320, 60)];
+    
+    
+    UIBarButtonItem *titleButton = [[UIBarButtonItem alloc]initWithTitle:@"Button" style:UIBarButtonItemStyleDone target:self action:@selector(handleButton:)];
+    
+    NSArray *segments = [NSArray arrayWithObjects:@"a",@"b", @"c",nil];
+    UISegmentedControl *segment = [[UISegmentedControl alloc]initWithItems:segments];
+    segment.segmentedControlStyle = UISegmentedControlStyleBar;
+    segment.frame = CGRectMake(0, 0, 140, 44);
+    UIBarButtonItem *segmentButton =[[UIBarButtonItem alloc]initWithCustomView:segment];
+    
+    
+    UIBarButtonItem *space = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self         action:@selector(handleRefresh:)];
+    
+    
+    NSArray *items = [NSArray arrayWithObjects:titleButton, segmentButton,space,refreshButton,nil];
+    
+    [toolbar setItems:items];
+    
+    [self.view addSubview:toolbar];
+    
+    
+    
+    
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
